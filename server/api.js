e710ff06a42279c80ebc9ae2e5c6b9a336e0bfb9
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-07-05 07:41:33 +0300

    Set routers

--- server/api.js
+++ server/api.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const apiRouter = express.Router();
+const ideasRouter = require('./ideas.js')
+const meetingsRouter = require('./meetings.js')
+const minionsRouter = require('./minions.js')
 
-
+//setting routers
+apiRouter.use('/ideas', ideasRouter)
+apiRouter.use('/meetings', meetingsRouter)
+apiRouter.use('/minions', minionsRouter)
 
 module.exports = apiRouter;
