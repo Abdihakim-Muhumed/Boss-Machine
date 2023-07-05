22a6e3f55c7d9416f8dae5a604e6d66a666389bf
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-07-05 10:43:09 +0300

    Custom middleware function

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,16 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    if(! req.query[numWeeks] || !req.query[weeklyRevenue]){
+        res.status(400).send()
+    }
+    if(typeof req.query.numWeeks != 'number' || typeof req.query.weeklyRevenue != 'number'){
+        res.status(400).send()
+    }
+    const totalValue = req.query[numWeeks] * req.query[weeklyRevenue]
+    if(totalValue < 1000000){
+        res.status(400).send("Not a million idea")
+    }
+    next()
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
