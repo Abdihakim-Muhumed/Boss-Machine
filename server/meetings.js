664f125aba053fb9088abd5d6173c56698b15cea
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-07-05 10:07:55 +0300

    Added CRUD endpoints for meetings

---
+++ server/meetings.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const meetingsRouter = express.Router()
+const {
+    createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+} = require('./db.js')
+
+//GET all meetings
+meetingsRouter.get('/', (req, res) => {
+    const meetings = getAllFromDatabase('meetings');
+    if(meetings){
+        res.send(meetings)
+    }
+})
+
+//POST a new meeting
+meetingsRouter.post('/', (req, res) => {
+    const meeting = createMeeting()
+    const newMeeting = addToDatabase('meetings', meeting)
+    if(!newMeeting){
+        res.status(500).send()
+    }
+    res.send(newMeeting);
+})
+
+//DELETE all meetings
+meetingsRouter.delete('/', (req, res) => {
+    const deleted = deleteAllFromDatabase('meetings')
+    if(!deleted){
+        res.status(500).send()
+    }
+    res.status(200).send()
+})
+
+module.exports = meetingsRouter
